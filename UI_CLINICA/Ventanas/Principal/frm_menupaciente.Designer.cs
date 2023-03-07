93ac82633281908ceb7ac1e416893f938b96664a
ARAYA LEIVA <jeison.araya@uamcr.net>
2023-03-07 14:26:04 -0600

    Cambio 3

--- UI_CLINICA/Ventanas/Principal/frm_menupaciente.Designer.cs
+++ UI_CLINICA/Ventanas/Principal/frm_menupaciente.Designer.cs
@@ -63,7 +63,7 @@ namespace UI_CLINICA.Ventanas.Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Red;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.msPaciente);
             this.MainMenuStrip = this.msPaciente;
