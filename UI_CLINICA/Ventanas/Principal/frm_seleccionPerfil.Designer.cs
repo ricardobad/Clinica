007ae605f3da8f3994145def3303b4cc4a3613b6
Ricardo_Badilla <ricardo.badilla@uamcr.net>
2023-04-15 19:49:38 -0600

    Se agrega !

--- UI_CLINICA/Ventanas/Principal/frm_seleccionPerfil.Designer.cs
+++ UI_CLINICA/Ventanas/Principal/frm_seleccionPerfil.Designer.cs
@@ -50,9 +50,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.label1.Location = new System.Drawing.Point(56, 341);
+            this.label1.Location = new System.Drawing.Point(84, 525);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(367, 28);
+            this.label1.Size = new System.Drawing.Size(557, 43);
             this.label1.TabIndex = 2;
             this.label1.Text = "Seleccione su perfil de ingreso";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -62,9 +63,10 @@
             this.btnIngresoColaborador.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIngresoColaborador.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresoColaborador.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(150)))), ((int)(((byte)(191)))));
-            this.btnIngresoColaborador.Location = new System.Drawing.Point(262, 551);
+            this.btnIngresoColaborador.Location = new System.Drawing.Point(393, 848);
+            this.btnIngresoColaborador.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIngresoColaborador.Name = "btnIngresoColaborador";
-            this.btnIngresoColaborador.Size = new System.Drawing.Size(201, 37);
+            this.btnIngresoColaborador.Size = new System.Drawing.Size(302, 57);
             this.btnIngresoColaborador.TabIndex = 117;
             this.btnIngresoColaborador.Text = "Colaborador";
             this.btnIngresoColaborador.UseVisualStyleBackColor = true;
@@ -75,9 +77,10 @@
             this.btnIngresoPaciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIngresoPaciente.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresoPaciente.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(150)))), ((int)(((byte)(191)))));
-            this.btnIngresoPaciente.Location = new System.Drawing.Point(33, 551);
+            this.btnIngresoPaciente.Location = new System.Drawing.Point(50, 848);
+            this.btnIngresoPaciente.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIngresoPaciente.Name = "btnIngresoPaciente";
-            this.btnIngresoPaciente.Size = new System.Drawing.Size(201, 37);
+            this.btnIngresoPaciente.Size = new System.Drawing.Size(302, 57);
             this.btnIngresoPaciente.TabIndex = 118;
             this.btnIngresoPaciente.Text = "Paciente";
             this.btnIngresoPaciente.UseVisualStyleBackColor = true;
@@ -90,20 +93,22 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.label2.Location = new System.Drawing.Point(77, 12);
+            this.label2.Location = new System.Drawing.Point(116, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(895, 56);
+            this.label2.Size = new System.Drawing.Size(497, 85);
             this.label2.TabIndex = 120;
-            this.label2.Text = "Bienvenidos a Photoshop con ricardo";
+            this.label2.Text = "Bienvenidos !";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(150)))), ((int)(((byte)(191)))));
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(87, 64);
+            this.pictureBox3.Location = new System.Drawing.Point(130, 98);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(303, 265);
+            this.pictureBox3.Size = new System.Drawing.Size(454, 408);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 119;
             this.pictureBox3.TabStop = false;
@@ -112,9 +117,10 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(262, 381);
+            this.pictureBox2.Location = new System.Drawing.Point(393, 586);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(201, 151);
+            this.pictureBox2.Size = new System.Drawing.Size(302, 232);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
@@ -123,9 +129,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(33, 381);
+            this.pictureBox1.Location = new System.Drawing.Point(50, 586);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(201, 151);
+            this.pictureBox1.Size = new System.Drawing.Size(302, 232);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -134,9 +141,10 @@
             // 
             this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
-            this.btnCerrar.Location = new System.Drawing.Point(468, 12);
+            this.btnCerrar.Location = new System.Drawing.Point(702, 18);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(25, 25);
+            this.btnCerrar.Size = new System.Drawing.Size(38, 38);
             this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnCerrar.TabIndex = 121;
             this.btnCerrar.TabStop = false;
@@ -144,11 +152,11 @@
             // 
             // frm_seleccionPerfil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(58)))), ((int)(((byte)(89)))));
-            this.ClientSize = new System.Drawing.Size(505, 617);
+            this.ClientSize = new System.Drawing.Size(758, 949);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox3);
@@ -159,6 +167,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frm_seleccionPerfil";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Seleccion de Perfil";
