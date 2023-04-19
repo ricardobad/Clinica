eba3d9a879233f32157552199cee2a8ca4d490e0
DaniRivP <120041429+DaniRivP@users.noreply.github.com>
2023-04-18 23:21:27 -0600

    PruebasDaniel

---
+++ BLL_Clinica/Catalogos/cls_Consultorios_BLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_Clinica.Catalogos
+{
+    public class cls_Consultorios_BLL
+    {
+        //Pruebas
+    }
+}
