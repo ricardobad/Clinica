ebc527cd74c78cda553dfa3d47f25b12f30be0a4
JeisonA210 <jeison.araya@uamcr.net>
2023-04-21 01:23:44 -0600

    ADICCIONES COMPLETO

--- BLL_Clinica/Catalogos/cls_Adicciones_BLL.cs
+++ BLL_Clinica/Catalogos/cls_Adicciones_BLL.cs
@@ -15,37 +15,145 @@ namespace BLL_Clinica.Catalogos
     {
         #region VARIABLES GLOBALES
 
-        cls_Adicciones_DAL Obj_Adicciones = new cls_Adicciones_DAL();
+        cls_Adicciones_DAL Obj_Adicciones_DAL = new cls_Adicciones_DAL();
         cls_BD_DAL Obj_BD_DAL = new cls_BD_DAL();
         cls_BD_BLL Obj_BD_BLL = new cls_BD_BLL();
 
         ///comentar
         #endregion
 
-        public void listar_Adicciones (ref cls_Adicciones_DAL Obj_Adicciones)
+
+
+
+
+        public void listar_Adicciones(ref cls_Adicciones_DAL Obj_Adicciones_DAL)
         {
 
-            //aqui se llama el Store Procedure que esta en el APP.Config
-
-            Obj_BD_DAL.sNomSp = ConfigurationManager.AppSettings["listar_adicciones"].ToString();
-
-            //aqui abajo se pone el nombrre de la tabla
-            Obj_BD_DAL.sNomTabla = "T_ADICCIONES";
-
-            Obj_BD_BLL.ExecDataAdapter(ref Obj_BD_DAL);
-
-            if (Obj_BD_DAL.sMsjError == string.Empty)
+            try
             {
-                Obj_Adicciones.sMsjError = string.Empty;
-                Obj_Adicciones.DtDatos = Obj_BD_DAL.obj_ds.Tables[0];
+                cls_BD_II_DAL Obj_BD_DAL = new cls_BD_II_DAL();
+                cls_BD_II_BLL Obj_BD_BLL = new cls_BD_II_BLL();
+
+                Obj_BD_DAL.sNombreDataTable = "T_ADICCIONES";
+
+                if (Obj_Adicciones_DAL.Descripcion == string.Empty)
+                {
+
+                    Obj_BD_DAL.sNobreSP = "dbo.SP_listar_adicciones";
+
+                    Obj_BD_DAL.dtParametros = null;
+
+
+
+                }
+                else
+                {
+                    Obj_BD_DAL.sNobreSP = "dbo.SP_FILTRAR_ADICCIONES";
+
+                    Obj_BD_BLL.CrearDTParametros(ref Obj_BD_DAL);
+
+                    Obj_BD_DAL.dtParametros.Rows.Add("@Filtro", "7", Obj_Adicciones_DAL.Descripcion);
+                }
+
+
+                Obj_BD_BLL.ExecDataAdapter(ref Obj_BD_DAL);
+
+                Obj_Adicciones_DAL.dsAdicciones = Obj_BD_DAL.dsDatos;
+                Obj_Adicciones_DAL.sMsjError = Obj_BD_DAL.sMsjError;
+
+
+
+
             }
-            else
+            catch (Exception ex)
             {
-                Obj_Adicciones.sMsjError = Obj_BD_DAL.sMsjError;
-                Obj_Adicciones.DtDatos = null;
+                Obj_Adicciones_DAL.sMsjError = ex.Message.ToString().Trim();
+
             }
 
         }
 
-    }
+
+
+        public void Modificar_Adicciones(ref cls_Adicciones_DAL Obj_Adicciones_DAL)
+            {
+            try
+            {
+                cls_BD_II_DAL Obj_BD_DAL = new cls_BD_II_DAL();
+                cls_BD_II_BLL Obj_BD_BLL = new cls_BD_II_BLL();
+
+                Obj_BD_DAL.sNobreSP = "dbo.SP_modificar_adicciones";
+
+                Obj_BD_BLL.CrearDTParametros(ref Obj_BD_DAL);
+
+                Obj_BD_DAL.dtParametros.Rows.Add("@ID_Adicciones", "1", Obj_Adicciones_DAL.ID_Adiccion);
+                Obj_BD_DAL.dtParametros.Rows.Add("@ID_Doctor", "1", Obj_Adicciones_DAL.ID_Doctor);
+                Obj_BD_DAL.dtParametros.Rows.Add("@Descripcion", "7", Obj_Adicciones_DAL.Descripcion);
+                Obj_BD_DAL.dtParametros.Rows.Add("@Estado", "9", Obj_Adicciones_DAL.booEstado);
+
+                Obj_BD_DAL.sIndAxn = "NORMAL";
+
+                Obj_BD_BLL.ExecCommand(ref Obj_BD_DAL);
+
+                Obj_Adicciones_DAL.sMsjError = Obj_BD_DAL.sMsjError;
+                Obj_Adicciones_DAL.cIndAxn = 'M';
+
+            }
+            catch (Exception ex)
+            {
+                Obj_Adicciones_DAL.cIndAxn = 'M';
+                Obj_Adicciones_DAL.sMsjError = ex.Message.ToString().Trim();
+
+            }
+
+        }
+
+
+        public void Insertar_Adicciones(ref cls_Adicciones_DAL Obj_Adicciones_DAL)
+        {
+            try
+            {
+                cls_BD_II_DAL Obj_BD_DAL = new cls_BD_II_DAL();
+                cls_BD_II_BLL Obj_BD_BLL = new cls_BD_II_BLL();
+
+                Obj_BD_DAL.sNobreSP = "dbo.SP_insertar_adicciones";
+
+                Obj_BD_BLL.CrearDTParametros(ref Obj_BD_DAL);
+
+                Obj_BD_DAL.dtParametros.Rows.Add("@ID_Doctor", "1", Obj_Adicciones_DAL.ID_Doctor);
+                Obj_BD_DAL.dtParametros.Rows.Add("@Descripcion", "7", Obj_Adicciones_DAL.Descripcion);
+                Obj_BD_DAL.dtParametros.Rows.Add("@Estado", "9", Obj_Adicciones_DAL.booEstado);
+
+
+
+                Obj_BD_DAL.sIndAxn = "IDENTITY";
+
+                Obj_BD_BLL.ExecCommand(ref Obj_BD_DAL);
+
+                Obj_Adicciones_DAL.sMsjError = Obj_BD_DAL.sMsjError;
+                if (Obj_Adicciones_DAL.sMsjError == string.Empty)
+                {
+                    Obj_Adicciones_DAL.ID_Adiccion = Convert.ToInt32(Obj_BD_DAL.sValorScalar);
+                    Obj_Adicciones_DAL.cIndAxn = 'M';
+                }
+                else
+                {
+                    Obj_Adicciones_DAL.cIndAxn = 'N';
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Obj_Adicciones_DAL.cIndAxn = 'N';
+                Obj_Adicciones_DAL.sMsjError = ex.Message.ToString().Trim();
+
+            }
+
+        }
+
+
+
+    
+}
 }
--- DAL_Clinica/Catalogos/cls_Adicciones_DAL.cs
+++ DAL_Clinica/Catalogos/cls_Adicciones_DAL.cs
@@ -13,13 +13,18 @@ namespace DAL
         private int _ID_Adiccion, _ID_Doctor;
         private string _sMsjError, _Descripcion;
         private DataTable _dtDatos, _dtParametros;
+        private char _cIndAxn;
+        private bool _booEstado;
 
+        private DataSet _dsAdicciones;
         public int ID_Adiccion { get => _ID_Adiccion; set => _ID_Adiccion = value; }
         public int ID_Doctor { get => _ID_Doctor; set => _ID_Doctor = value; }
         public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
         public string sMsjError { get => _sMsjError; set => _sMsjError = value; }
         public DataTable DtDatos { get => _dtDatos; set => _dtDatos = value; }
         public DataTable DtParametros { get => _dtParametros; set => _dtParametros = value; }
-
+        public char cIndAxn { get => _cIndAxn; set => _cIndAxn = value; }
+        public bool booEstado { get => _booEstado; set => _booEstado = value; }
+        public DataSet dsAdicciones { get => _dsAdicciones; set => _dsAdicciones = value; }
     }
 }
