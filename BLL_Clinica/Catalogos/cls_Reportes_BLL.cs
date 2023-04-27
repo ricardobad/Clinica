bcc7a971343739443527ec4488a508e16dba16d9
Ricardo_Badilla <ricardo.badilla@uamcr.net>
2023-04-27 16:04:07 -0600

    Reportes listo

--- BLL_Clinica/Catalogos/cls_Reportes_BLL.cs
+++ BLL_Clinica/Catalogos/cls_Reportes_BLL.cs
@@ -64,6 +64,56 @@ namespace BLL_Clinica.Catalogos
 
         }
 
+        public void CitasIdentificacion(ref cls_Reportes_DAL Obj_Reportes_DAL)
+        {
+            Obj_BD_DAL.sNomSp = ConfigurationManager.AppSettings["reporte_citas_identificacion"].ToString();
+            Obj_BD_BLL.crearDTparametros(ref Obj_BD_DAL);
+            Obj_BD_DAL.obj_dtParametros.Rows.Add("@fecha_inicial", "8", Obj_Reportes_DAL.FechaInicial);
+            Obj_BD_DAL.obj_dtParametros.Rows.Add("@fecha_final", "8", Obj_Reportes_DAL.FechaFinal);
+            Obj_BD_DAL.obj_dtParametros.Rows.Add("@Identificacion", "7", Obj_Reportes_DAL.sIdentificacion);
+
+            Obj_BD_DAL.sNomTabla = "T_CI_IDEN";
+
+            Obj_BD_BLL.ExecDataAdapter(ref Obj_BD_DAL);
+            if (Obj_BD_DAL.sMsjError == string.Empty)
+            {
+                Obj_Reportes_DAL.sMsjError = string.Empty;
+                Obj_Reportes_DAL.DtDatos = Obj_BD_DAL.obj_ds.Tables[0];
+            }
+            else
+            {
+                Obj_BD_DAL.sMsjError = Obj_BD_DAL.sMsjError;
+                Obj_Reportes_DAL.DtDatos = null;
+            }
+
+        }
+
+
+        public void CitasMedico(ref cls_Reportes_DAL Obj_Reportes_DAL)
+        {
+            Obj_BD_DAL.sNomSp = ConfigurationManager.AppSettings["reporte_citas_medico"].ToString();
+            Obj_BD_BLL.crearDTparametros(ref Obj_BD_DAL);
+            Obj_BD_DAL.obj_dtParametros.Rows.Add("@fecha_inicial", "8", Obj_Reportes_DAL.FechaInicial);
+            Obj_BD_DAL.obj_dtParametros.Rows.Add("@fecha_final", "8", Obj_Reportes_DAL.FechaFinal);
+            Obj_BD_DAL.obj_dtParametros.Rows.Add("@Carnet", "7", Obj_Reportes_DAL.sCarnet);
+
+            Obj_BD_DAL.sNomTabla = "T_CI_Carnet";
+
+            Obj_BD_BLL.ExecDataAdapter(ref Obj_BD_DAL);
+            if (Obj_BD_DAL.sMsjError == string.Empty)
+            {
+                Obj_Reportes_DAL.sMsjError = string.Empty;
+                Obj_Reportes_DAL.DtDatos = Obj_BD_DAL.obj_ds.Tables[0];
+            }
+            else
+            {
+                Obj_BD_DAL.sMsjError = Obj_BD_DAL.sMsjError;
+                Obj_Reportes_DAL.DtDatos = null;
+            }
+
+        }
+
+
 
     }
 }
